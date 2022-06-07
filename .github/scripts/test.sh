583c87c9c5b2e3f9084072f8b8d3bb3a4a34c2ec
Marie-Terrell <marie.terrell@cfa.harvard.edu>
2022-06-07 14:25:14 -0400

    Move the check and install before the submodule test

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -30,21 +30,21 @@ if [ -n "${XSPECVER}" ]; then XSPECTEST="-x -d"; fi
 if [ -n "${FITS}" ] ; then FITSTEST="-f ${FITS}"; fi
 smokevars="${XSPECTEST} ${FITSTEST} -v 3"
 
-if [ ${TEST} == submodule ]; then
-    conda install -yq pytest-cov;
-
-    python setup.py -q test -a "--cov sherpa --cov-report xml" || exit 1;
-fi
-
-# Run smoke test
-cd /home
-sherpa_smoke ${smokevars} || exit 1
-
 # Set command to run doctests if XSPEC and test data are present
 if [ -n "${XSPECVER}" ] && [ ${TEST} == "package" -o ${TEST} == "submodule" ]; then
     DOCTEST="--doctest-plus";
     conda install -yq pytest-doctestplus;
 fi
+
+if [ ${TEST} == submodule ]; then
+    conda install -yq pytest-cov;
+
+    python setup.py -q test -a "${DOCTEST} --cov sherpa --cov-report xml" || exit 1;
+fi
+
+# Run smoke test
+cd /home
+sherpa_smoke ${smokevars} || exit 1
 
 # Run regression tests using sherpa_test
 if [ ${TEST} == package ] || [ ${TEST} == none ]; then
